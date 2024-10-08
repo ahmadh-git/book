c40f719ff6b773274933e4014a4f21c42cf6009e
Ahmad Hassouneh <ahmadhassouneh0@gmail.com>
2024-10-08 13:46:13 -0400

    Final Version

--- main.py
+++ main.py
@@ -1,57 +1,89 @@
-# Step 1: Import the package
 import csv
+import math
+import os
 import urllib.request
+import pathlib
+import re
+
 from bs4 import BeautifulSoup
+from funcs import single_book, clean_tags, get_html_string
 
-# Step 2: Assign the URL to variable
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
- 
-# Step 3: First open, then read the the HTML
-text = urllib.request.urlopen(url).read().decode("utf-8")
-soup = BeautifulSoup(text, "html.parser")
-paragraphs = soup.find_all('p')
+# Function to sanitize filenames by replacing invalid characters
+def sanitize_filename(filename):
+    # Replace any characters that are not valid in file names with an underscore
+    return re.sub(r'[<>:"/\\|?*#]', '_', filename)
 
-#Step 4: Print the HTML code
-table_elements = soup.find("table").find_all("tr")
+def main():
+    html_string = get_html_string("https://books.toscrape.com/catalogue/category/books_1/index.html")
+    cat_path = "https://books.toscrape.com/catalogue/category/books/"
+    book_path = "https://books.toscrape.com/catalogue"
+    soup = BeautifulSoup(html_string, "html.parser")
+    categories = soup.find("div", {"class": "side_categories"}).find_all("a")
+    category_strings = []
+    
+    for x in categories:
+        category_strings.append(clean_tags(str(x).strip()).strip())
 
-base_path = "https://books.toscrape.com/"
-upc = table_elements[0].find("td").get_text()
-price_inc_tax = table_elements[3].find("td").get_text()
-price_exc_tax = table_elements[2].find("td").get_text()
-availability = table_elements[5].find("td").get_text()
-imageurl = base_path + soup.img["src"][6:len(soup.img["src"])]
+    category_strings = category_strings[1:len(category_strings)]  # Skipping the first 'Books' category
+    catInt = 2
+    category_title = ""
 
-book_title = soup.find("ul").find_all("li")[3].get_text()
-book_category = soup.find("ul").find_all("li")[2].get_text()
-review = paragraphs[2]["class"][1]
-product_desc = paragraphs[3]
+    for y in category_strings:
+        url = cat_path + y.lower().replace(" ", "-") + "_" + str(catInt) + "/index.html"
+        html_string = get_html_string(url)
+        soup = BeautifulSoup(html_string, "html.parser")
+        result_num = clean_tags(str(soup.find_all("strong")[1]))
+        product_page_tags = soup.find_all("div", {"class": "image_container"})
+        page_paths = []
+        category_title = clean_tags(str(soup.find("h1")))
 
-print(book_title, book_category, imageurl, review, product_desc)
+        for x in product_page_tags:
+            curr = str(x.find("a").get("href"))
+            page_paths.append(curr[8:len(curr)])
 
-#Create a Dictionary
+        # Handling multiple pages of products
+        if int(result_num) > 20:
+            number_of_pages = math.ceil(int(result_num) / 20)
+            current_page = 2
+            while current_page <= number_of_pages:
+                url2 = cat_path + y.lower().replace(" ", "-") + "_" + str(catInt) + "/page-" + str(current_page) + ".html"
+                html_string2 = get_html_string(url2)
+                soup2 = BeautifulSoup(html_string2, "html.parser")
+                product_page_tags2 = soup2.find_all("div", {"class": "image_container"})
+                for z in product_page_tags2:
+                    curr = str(z.find("a").get("href"))
+                    page_paths.append(curr[8:len(curr)])
+                current_page += 1
+        
+        cat_data = []
+        for b in page_paths:
+            cat_data.append(single_book(False, book_path + b))
 
-dictionary = {
-        "product_page_url": imageurl, 
-        "universal_product_code (upc)": upc,
-        "book_title": book_title,
-        "price_including_tax": price_inc_tax,
-        "price_excluding_tax": price_exc_tax,
-        "quantity_available": availability,
-        "product_description": product_desc,
-        "category": book_category,
-        "review_rating": review,
-        "image_url": imageurl
-    }
+        path = str(pathlib.Path().resolve())
+        
+        # Create directories if they don't exist
+        if not os.path.exists(path + "/data"):
+            os.mkdir(path + "/data")
+        if not os.path.exists(path + "/data/img"):
+            os.mkdir(path + "/data/img")
 
-# use dictionary
-temp = [dictionary]
-fields = temp[0].keys()
-filename = "bookScrape_" + book_title.replace(" ", "_") + ".csv"
-with open(filename, 'w') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-    writer.writeheader()
-    writer.writerows(temp)
+        # Save images with sanitized filenames
+        for c in cat_data:
+            sanitized_category_title = sanitize_filename(category_title.replace(" ", "_"))
+            sanitized_book_title = sanitize_filename(c["book_title"].replace(" ", "_"))
+            image_path = path + f"/data/img/{sanitized_category_title}_{sanitized_book_title}_img.png"
+            
+            # Download image and handle invalid characters in URLs
+            urllib.request.urlretrieve(c["image_url"], image_path)
 
-#Define Categories
-categories = soup.find_all("ul")
-print(categories)
+        # Save data to CSV
+        fields = cat_data[0].keys()
+        filename = "categoryScrape_" + sanitize_filename(category_title.replace(" ", "_")) + ".csv"
+        with open(path + "/data/" + filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fields)
+            writer.writeheader()
+            writer.writerows(cat_data)
+
+        catInt += 1
+
+main()
